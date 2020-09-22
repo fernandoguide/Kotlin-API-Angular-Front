f40d8b6c65d1549f6e41b2d6fcee5fe21c75210f
Fernando <fernando-guide@hotmail.com>
2020-09-22 13:25:36 -0300

    criando app angular

---
+++ notes/src/app/app.module.ts
@@ -0,0 +1,24 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { AuthRoutingModule } from './auth-routing.module';
+import { NoteModule } from './note/note.module';
+
+@NgModule({
+  declarations: [
+    AppComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    NgbModule,
+    AuthRoutingModule,
+    NoteModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
